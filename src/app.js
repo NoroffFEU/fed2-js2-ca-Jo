3c7c416f0b764186e2510e02ca53e723816c0068
DESKTOP-00P3NR3\jotan <jotanvo@gmail.com>
2024-09-28 00:17:13 +0200

    Trying to fix app.js not finding css and router files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,3 @@
-import "./css/style.css";
-import "/src/index.css";
-
-import router from "./js/router";
-
+import "./css/style.css"; // This should remain the same
+import router from "./js/router/index.js"; // Update this to point to the correct router file
 await router(window.location.pathname);
